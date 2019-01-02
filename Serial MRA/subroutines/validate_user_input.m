39987a2e4475e8bc49ff77e82850b2a5c538f4f8
Lewis <lblake@mines.edu>
2019-01-02 13:09:47 -0700

    Update validate_user_input.m

--- Serial MRA/subroutines/validate_user_input.m
+++ Serial MRA/subroutines/validate_user_input.m
@@ -10,27 +10,27 @@ function [] = validate_user_input(NUM_LEVELS_M, NUM_PARTITIONS_J, NUM_KNOTS_r, o
 % Error handling on all input arguments except for dataSource which is
 % checked within load_data.m
 if ~isa(NUM_LEVELS_M, 'double') || isinf(NUM_LEVELS_M) || (~isinf(NUM_LEVELS_M) && floor(NUM_LEVELS_M) ~= NUM_LEVELS_M)
-    error('myfuns:user_input_error_handling: NUM_LEVELS_M must be a positive integer of class double.')
+    error('myfuns:validate_user_input: NUM_LEVELS_M must be a positive integer of class double.')
 elseif ~isa(NUM_PARTITIONS_J, 'double') || (NUM_PARTITIONS_J ~= 2 && NUM_PARTITIONS_J ~= 4) || isinf(NUM_PARTITIONS_J) || (~isinf(NUM_PARTITIONS_J) && floor(NUM_PARTITIONS_J) ~= NUM_PARTITIONS_J)
-    error('myfuns:user_input_error_handling: NUM_PARTITIONS_J must be either 2 or 4.')
+    error('myfuns:validate_user_input: NUM_PARTITIONS_J must be either 2 or 4.')
 elseif ~isa(NUM_KNOTS_r, 'double') || (~isinf(NUM_KNOTS_r) && floor(NUM_KNOTS_r) ~= NUM_KNOTS_r) || isinf(NUM_KNOTS_r)
-    error('myfuns:user_input_error_handling: NUM_KNOTS_r must be a positive integer of class double.')
+    error('myfuns:validate_user_input: NUM_KNOTS_r must be a positive integer of class double.')
 elseif ~isa(offsetPercentage, 'double') || (offsetPercentage < 0 || offsetPercentage >= 1) || isinf(offsetPercentage)
-    error('myfuns:user_input_error_handling: offsetPercentage must be between 0 and 1.')
+    error('myfuns:validate_user_input: offsetPercentage must be between 0 and 1.')
 elseif ~isa(nXGrid, 'double') || (~isinf(nXGrid) && floor(nXGrid) ~= nXGrid) || isinf(nXGrid) || nXGrid < 1
-    error('myfuns:user_input_error_handling: nXGrid must be a positive integer of class double.')
+    error('myfuns:validate_user_input: nXGrid must be a positive integer of class double.')
 elseif ~isa(nYGrid, 'double') || (~isinf(nYGrid) && floor(nYGrid) ~= nYGrid) || isinf(nYGrid) || nYGrid < 1
-    error('myfuns:user_input_error_handling: nYGrid must be a positive integer of class double.')
+    error('myfuns:validate_user_input: nYGrid must be a positive integer of class double.')
 elseif ~islogical(displayPlots)
-    error('myfuns:user_input_error_handling: displayPlots must be a boolean.')
+    error('myfuns:validate_user_input: displayPlots must be a boolean.')
 elseif ~islogical(savePlots)
-    error('myfuns:user_input_error_handling: savePlots must be a boolean.')
+    error('myfuns:validate_user_input: savePlots must be a boolean.')
 elseif ~islogical(verbose)
-    error('myfuns:user_input_error_handling: verbose must be a boolean.')
+    error('myfuns:validate_user_input: verbose must be a boolean.')
 elseif ~ischar(resultsFilePath)
-    error('myfuns:user_input_error_handling: resultsFilePath must be a char. See default.')
+    error('myfuns:validate_user_input: resultsFilePath must be a char. See default.')
 elseif ~ischar(plotsFilePath)
-    error('myfuns:user_input_error_handling: plotsFilePath must be a char. See default.')
+    error('myfuns:validate_user_input: plotsFilePath must be a char. See default.')
 end
 
 end
