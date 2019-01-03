1d1c2cd0bc53d439e1d9199d2312635a30411a80
Lewis <lblake@mines.edu>
2019-01-03 09:31:28 -0700

    Update user_input.m
    Made user inputs for each calculationType more explicit

--- Serial MRA/user_input.m
+++ Serial MRA/user_input.m
@@ -13,33 +13,31 @@
 
 %% Workspace cleanup and add file path to subroutines
 clear all; addpath('subroutines');
-%% User Input: Data Type and Model Parameters
-
+%% User Input: dataSource and calculationType
 % Choose dataSource. Set to be a char corresponding to a case within the
 % swtich clause of load_data.m
 % Included data sets are 'satellite' and 'simulated'.
 dataSource = 'satellite'; % Default is 'satellite'.
-
 % Choose calculationType: | 'prediction | 'optimize' | 'likelihood' |
 calculationType = 'likelihood'; % Default is likelihood
+%% Inputs relevant for any calculationType
 % Below are the choices for M, J, and r as denoted in Katzfuss 2017.
 % To estimate NUM_LEVELS_M, see find_num_levels_suggested_required.m. J must either be 2 or 4.
 NUM_LEVELS_M = 9; % Total number of levels. Set to be a natural number. Deafult is 9.
 NUM_PARTITIONS_J = 2; % Number of partitions of each region at each level. Set to be 2 or 4. Default is 2.
 NUM_KNOTS_r = 64; % Number of knots per partition. Default is 64.
 offsetPercentage = 0.01; % Offset percentage from partition boundaries. Default is 0.01.
-
+verbose = true; % Boolean variable indicating whether to display progress indicators. Default is true.
+resultsFilePath = './Results/';  % By default, results from all routines are saved in the Results folder.
 
 %% Inputs relevant if calculationType = 'prediction'.
 % That is, these inputs are only used in prediction mode.
 displayPlots = true; % Boolean variable indicating whether to display plots when executing the 'prediction' calculationType. Default is true.
 savePlots = false; % Boolean variable indicating whether to save plots when executing the 'prediction' calculationType. Default is false.
-verbose = true; % Boolean variable indicating whether to display progress indicators. Default is true.
 nXGrid = 200; % Number of prediction grid points in x-direction. Only required for 'prediction' calculationType. Default is 200.
 nYGrid = 200; % Number of prediction grid points in y-direction. Only required for 'prediction' calculationType. Default is 200.
 % Optional: select file paths to save prediction results and plots if
 % predicting. e.g. resultsFilesPath = '/Users/JerryGarcia/Desktop/';
-resultsFilePath = './Results/';  % By default, prediction results are dumped into Results folder
 plotsFilePath = './Plots/'; % By default, plots are dumped into Plots folder
 
 %% Inputs relevant if calculationType = 'optimize'
